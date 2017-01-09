0219e5b274ecb014062aa0983fcf76dad310fd5f
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-01-09 21:17:37 +0100

    Implementing MineBoard component

--- src/ui/react/MineBoard.js
+++ src/ui/react/MineBoard.js
@@ -4,6 +4,13 @@ import './MineBoard.css';
 const MineBoard = ({ mineBoard, onMineClick }) => (
     <div className="mineBoard">
         <div className="mineBoardText">Splendid!</div>
+        {mineBoard.rows.map((row, rowIndex) => (
+            <div key={rowIndex}>
+                {row.map((item, itemIndex) => (
+                    <MineBox key={itemIndex} item={item} onMineClick={() => onMineClick(rowIndex, itemIndex)} />
+                ))}
+            </div>
+        ))}
     </div>
 );
 
