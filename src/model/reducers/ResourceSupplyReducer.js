ff9e58b1daa998c1a578cdeb24690785e93f16eb
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-01-09 13:13:06 +0100

    Updating ResourceSupplyReducer to match intended logic.

--- src/model/reducers/ResourceSupplyReducer.js
+++ src/model/reducers/ResourceSupplyReducer.js
@@ -14,8 +14,32 @@ const ResourceSupplyReducer = (state, action) => {
 
     return {
         ...state,
-        stacks: state.stacks.map(stack => ResourceStackReducer(stack, action))
+        stacks: updateAllowStackSelection(state.stacks.map(stack => ResourceStackReducer(stack, action)))
     }
+};
+
+const updateAllowStackSelection = (stacks = []) => {
+    let selectedResourceStacks = stacks.filter(stack => (stack.selectedCount > 0));
+    let totalSelectedResources =
+        selectedResourceStacks.reduce(
+            (accumulator, stack) => (accumulator + stack.selectedCount),
+            /* initialValue */ 0);
+
+    let allowSelectionUpdateFn;
+    if (selectedResourceStacks.length == 0) {
+        allowSelectionUpdateFn = (_) => (true);
+    } else if (selectedResourceStacks.length == 1) {
+        allowSelectionUpdateFn = (_) => (totalSelectedResources < 2);
+    } else if (selectedResourceStacks.length == 2) {
+        allowSelectionUpdateFn = (stack) => (!selectedResourceStacks.includes(stack));
+    } else {
+        allowSelectionUpdateFn = (_) => (false);
+    }
+
+    return stacks.map(stack => ({
+        ...stack,
+        allowSelection: allowSelectionUpdateFn(stack)
+    }));
 };
 
 export default ResourceSupplyReducer;
