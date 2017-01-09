daeee4289690b5be8cfff9f5f11d9cd1a9302391
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-01-09 22:19:56 +0100

    Randomly generating ResourceBox elements in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Config from './model/Config';
 import MineBox from './ui/react/MineBox';
 import ResourceBox from './ui/react/ResourceBox';
 import MineBoardComponent from './ui/components/MineBoardComponent';
@@ -7,11 +8,12 @@ import ResourcePanelComponent from './ui/components/ResourcePanelComponent';
 const App = () => (
     <div>
         <MineBox bg_color="green" fg_color="white" >
-            <ResourceBox count="1" bg_color="red" fg_color="white" />
-            <ResourceBox count="2" bg_color="blue" fg_color="white" />
-            <ResourceBox count="3" bg_color="darkgreen" fg_color="white" />
-            <ResourceBox count="4" bg_color="black" fg_color="white" />
-            <ResourceBox count="0" bg_color="white" fg_color="black" />
+            {Config.resourceTypes.map((resourceType) => (
+                <ResourceBox
+                    count={Math.floor(Math.random() * 9) + 1}
+                    bg_color={resourceType}
+                    fg_color="black" />
+            ))}
         </MineBox>
         <MineBoardComponent />
         <ResourcePanelComponent />
