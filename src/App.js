953e697cecab3c5c679239dee76a2e8cba035f13
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-01-11 14:10:28 +0100

    Putting SubmitButton into App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import MineBoardComponent from './ui/components/MineBoardComponent';
 import ResourcePanelComponent from './ui/components/ResourcePanelComponent';
+import SubmitButton from './ui/react/SubmitButton';
 import './App.css';
 
 const App = () => (
     <div className="appContainer">
         <div className="playArea">
-            <MineBoardComponent />
-            <ResourcePanelComponent />
+            <div className="centerArea">
+                <MineBoardComponent />
+                <ResourcePanelComponent />
+            </div>
+            <SubmitButton />
         </div>
     </div>
 );
