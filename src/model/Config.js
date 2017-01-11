e57f3b63b6990f38b3a1bb2adab2511825ac50b4
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-01-11 14:10:11 +0100

    Adding initial version of SubmitButton

--- src/model/Config.js
+++ src/model/Config.js
@@ -2,7 +2,8 @@ const Config = {
     resourceTypes: ["red", "cyan", "green", "yellow", "orange"],
     maxStackHeight: 4,
     mineBoardRows: 3,
-    mineBoardRowSize: 4
+    mineBoardRowSize: 4,
+    submitButtonText: "Get"
 };
 
 export default Config;
---
+++ src/ui/react/SubmitButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Config from '../../model/Config';
+
+const SubmitButton = () => (
+    <button>{Config.submitButtonText}</button>
+);
+
+export default SubmitButton;
