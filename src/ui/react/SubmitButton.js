58373a5a0beb5d31d425b7022836ffc594bd1fd7
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-01-11 14:35:40 +0100

    Adding PropTypes for SubmitButton

--- src/ui/react/SubmitButton.js
+++ src/ui/react/SubmitButton.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Config from '../../model/Config';
 
 const SubmitButton = ({ active, onClick }) => (
     <button disabled={!active} onClick={() => onClick()}>{Config.submitButtonText}</button>
 );
+
+SubmitButton.PropTypes = {
+    active: PropTypes.bool.isRequired,
+    onClick: PropTypes.func.isRequired
+};
 
 export default SubmitButton;
