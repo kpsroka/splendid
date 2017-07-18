c09dfe6ed2e99be853894f524a872b70ed7c3b0a
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-07-18 15:54:46 +0200

    Adding middleware to createStore call.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import App from './ui/App';
 import AppReducer from './model/reducers/AppReducer';
 import DefaultStatePromise from './model/DefaultState';
 import './index.css';
 
+const loggerMiddleware = createLogger();
+
 DefaultStatePromise
-    .then((state) => (createStore(AppReducer, state)))
+    .then((state) => (
+        createStore(AppReducer, state, applyMiddleware(thunkMiddleware, loggerMiddleware))))
     .then((store) => {
-        ReactDOM.render(
-            <Provider store={store}>
-                <App />
-            </Provider>,
-            document.getElementById('root')
-        )}
-    );
+      ReactDOM.render(
+          <Provider store={store}>
+            <App />
+          </Provider>,
+          document.getElementById('root')
+      )
+    }
+);
