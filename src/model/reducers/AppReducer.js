1b0af1e7bdd02ffb3764de8c44bb1f0a231530ce
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-07-18 16:06:29 +0200

    Temporarily disabling all reducers :)

--- src/model/reducers/AppReducer.js
+++ src/model/reducers/AppReducer.js
@@ -3,9 +3,9 @@ import ResourceSupplyReducer from './ResourceSupplyReducer';
 
 function AppReducer(state, action) {
     return {
-        mineBoard: state.mineBoard,
-        resourceSupply: ResourceSupplyReducer(state.resourceSupply, action),
-        hand: HandReducer(state, action)
+//        mineBoard: state.mineBoard,
+//        resourceSupply: ResourceSupplyReducer(state.resourceSupply, action),
+//        hand: HandReducer(state, action)
     }
 }
 
