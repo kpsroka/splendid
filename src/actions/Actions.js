e1e58b83fba52a56f04b533703dca27145b46028
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-07-19 13:11:06 +0200

    Adding SetUiMessage action.

--- src/actions/Actions.js
+++ src/actions/Actions.js
@@ -1,26 +1,50 @@
 import CreateGame from './async/CreateGame.js';
 
+const PrivateActions = {
+  SetUiMessage: (text, severity='INFO') => ({
+    type: '_SET_UI_MESSAGE',
+    text: text,
+    severity: severity
+  })
+};
+
 const Actions = {
   ChooseResourceFromStack: (resourceType) => ({
-    type: "CHOOSE_STACK_RESOURCE",
+    type: 'CHOOSE_STACK_RESOURCE',
     resourceType: resourceType
   }),
   ChooseMineFromBoard: (row, index) => ({
-    type: "CHOOSE_MINE",
+    type: 'CHOOSE_MINE',
     row: row,
     index: index
   }),
   GrabSelectedResources: () => ({
-    type: "GRAB_RESOURCES",
+    type: 'GRAB_RESOURCES',
   }),
   NewGame: (playerName, playerCount) => (
-      () => {
+      (dispatch) => {
+        dispatch(PrivateActions.SetUiMessage('Starting new game'));
+
         CreateGame(playerName, playerCount)
         .then(
-            response => response.json(),
-            error => console.log("Received error: " + error))
+            response => {
+              if (response.ok) {
+                return response.json();
+              } else {
+                throw new Error(`Failed to create new game`);
+              }
+            },
+            () => { throw new Error(`Network failure`); })
         .then(
-            (json) => console.log(json));
+            (json) => {
+              console.log(json);
+              dispatch(PrivateActions.SetUiMessage('Created new game'));
+            })
+        .catch(
+            error => {
+              dispatch(PrivateActions.SetUiMessage(error.message()));
+            }
+        );
       }
   )
 };
