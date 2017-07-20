93da286b9bc481a9ee28c1f43c416010ddcc320f
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-07-20 14:03:48 +0200

    Making PlayField a functional component.

--- src/ui/react/playfield/PlayField.js
+++ src/ui/react/playfield/PlayField.js
@@ -2,26 +2,24 @@ import React from 'react';
 import PlayerBoxComponent from '../../components/PlayerBoxComponent.js';
 import './PlayField.css';
 
-export default class PlayField extends React.PureComponent {
-  render() {
-    return (
-        <div className="PlayField-container">
-          <div className="PlayField-fieldArea">
-            <div className="PlayField-otherPlayersArea">
-              <PlayerBoxComponent playerIndex={2} />
-              <PlayerBoxComponent playerIndex={1} />
-            </div>
-            <div className="PlayField-fieldArea">
-            </div>
-            <div className="PlayField-otherPlayersArea">
-              <PlayerBoxComponent playerIndex={3} />
-              <PlayerBoxComponent playerIndex={4} />
-            </div>
+export default function PlayField() {
+  return (
+      <div className="PlayField-container">
+        <div className="PlayField-fieldArea">
+          <div className="PlayField-otherPlayersArea">
+            <PlayerBoxComponent playerIndex={2} />
+            <PlayerBoxComponent playerIndex={1} />
           </div>
-          <div className="PlayField-thisPlayerArea">
-            <PlayerBoxComponent playerIndex={0} />
+          <div className="PlayField-fieldArea">
+          </div>
+          <div className="PlayField-otherPlayersArea">
+            <PlayerBoxComponent playerIndex={3} />
+            <PlayerBoxComponent playerIndex={4} />
           </div>
         </div>
-    );
-  }
+        <div className="PlayField-thisPlayerArea">
+          <PlayerBoxComponent playerIndex={0} />
+        </div>
+      </div>
+  );
 }
