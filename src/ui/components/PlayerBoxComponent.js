4837a802f4b8c8a1fd112eae3722af53cfc83982
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-07-20 13:59:07 +0200

    Adding PlayerBoxComponent (populating name)

---
+++ src/ui/components/PlayerBoxComponent.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import PlayerBox from '../react/playfield/PlayerBox.js';
+
+
+function mapStateToProps(state, ownProps) {
+  if (state.players.length <= ownProps.playerIndex) {
+    return { disabled: true };
+  } else {
+    return {
+      disabled: false,
+      name: state.players[ownProps.playerIndex].name,
+      score: 0
+    };
+  }
+}
+
+const PlayerBoxComponent = connect(mapStateToProps)(PlayerBox);
+
+export default PlayerBoxComponent;
--- src/ui/react/playfield/PlayField.js
+++ src/ui/react/playfield/PlayField.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PlayerBoxComponent from '../../components/PlayerBoxComponent.js';
 import './PlayField.css';
 
 export default class PlayField extends React.PureComponent {
@@ -7,13 +8,18 @@ export default class PlayField extends React.PureComponent {
         <div className="PlayField-container">
           <div className="PlayField-fieldArea">
             <div className="PlayField-otherPlayersArea">
+              <PlayerBoxComponent playerIndex={2} />
+              <PlayerBoxComponent playerIndex={1} />
             </div>
             <div className="PlayField-fieldArea">
             </div>
             <div className="PlayField-otherPlayersArea">
+              <PlayerBoxComponent playerIndex={3} />
+              <PlayerBoxComponent playerIndex={4} />
             </div>
           </div>
           <div className="PlayField-thisPlayerArea">
+            <PlayerBoxComponent playerIndex={0} />
           </div>
         </div>
     );
