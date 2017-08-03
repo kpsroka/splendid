a779e6be3595ca42ee39d505c3b42d333557208b
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-08-03 17:33:04 +0200

    Correcting logic for resource selection.

--- src/model/reducers/BoardReducer.js
+++ src/model/reducers/BoardReducer.js
@@ -1,6 +1,6 @@
 // @flow
 
-import type { Resource, Board, Selection } from '../State.js';
+import type { Resource, Board } from '../State.js';
 import type { Action } from '../../actions/Actions.js';
 
 import { ActionTypes } from '../../actions/Actions.js';
@@ -8,14 +8,31 @@ import { ActionTypes } from '../../actions/Actions.js';
 export default function BoardReducer(board:Board, action:Action):Board {
   if (action.type === ActionTypes.ChooseResourceFromStack) {
     let selection = board.selection;
-    if (canSelectResource(selection, action.resourceType)) {
-      let newBoardSelection: Array<Resource> =
-          (selection.type === 'RESOURCE_SELECTION') ? selection.selection.slice() : [];
+    if (selection.type === 'FACTORY_SELECTION') {
+      return board;
+    } else if (selection.type === 'NO_SELECTION') {
+      return {
+        ...board,
+        selection: {
+          type: 'RESOURCE_SELECTION',
+          selection: [action.resourceType]
+        }
+      }
+    } else {
+      let oldResourceCount = countResources(selection.selection, action.resourceType);
+      let newResourceCount = getResourceCountAfterClick(
+          countResources(board.resources, action.resourceType),
+          oldResourceCount,
+          selection.selection,
+          action.resourceType);
 
-      if (countResources(newBoardSelection, action.resourceType) ===
-          countResources(board.resources, action.resourceType)) {
-        newBoardSelection = newBoardSelection.filter(x => x !== action.resourceType);
-      } else {
+      if (oldResourceCount === newResourceCount) {
+        return board;
+      }
+
+      let newBoardSelection: Array<Resource> =
+          selection.selection.filter(x => x !== action.resourceType);
+      for (let i = 0; i < newResourceCount; i++) {
         newBoardSelection.push(action.resourceType);
       }
 
@@ -33,27 +50,37 @@ export default function BoardReducer(board:Board, action:Action):Board {
           }
         };
       }
-    } else {
-      return board;
     }
   }
 
   return board;
 }
 
-function canSelectResource(selection:Selection, resourceType:Resource) {
-  if (selection.type === 'NO_SELECTION') {
-    return true;
-  } else if (selection.type === 'FACTORY_SELECTION') {
-    return false;
-  } else if (selection.type === 'RESOURCE_SELECTION') {
-    let selectedResourceSet = new Set(selection.selection);
-    return (selectedResourceSet.size === 0 || selectedResourceSet.size === 1 ||
-        (selectedResourceSet.size === 2 && !selectedResourceSet.has(resourceType)) ||
-        (selectedResourceSet.size === 3 && selectedResourceSet.has(resourceType)));
-  }
-}
-
 function countResources(resourceArray:Array<Resource>, resourceType:Resource):number {
   return resourceArray.filter(x => x === resourceType).length;
+}
+
+function getResourceCountAfterClick(
+    availableResourcesOfType:number,
+    selectedResourcesOfType:number,
+    selection:Array<Resource>,
+    resourceType:Resource):number {
+  if (availableResourcesOfType === 0) {
+    return 0;
+  } else if (selectedResourcesOfType === availableResourcesOfType) {
+    return 0;
+  } else if (selection.length === 0) {
+    return 1;
+  } else {
+    let selectedResourceTypes = new Set(selection);
+    if (selectedResourceTypes.size === 3) {
+      return 0;
+    } else if (selectedResourceTypes.size === 2) {
+      return selectedResourceTypes.has(resourceType) ? 0 : 1;
+    } else if (selectedResourceTypes.size === 1) {
+      return (selectedResourcesOfType > 0 || selection.length === 1) ? (selectedResourcesOfType + 1) % 4 : 0;
+    }
+  }
+
+  throw new Error("Should have exhausted all possibilities");
 }
