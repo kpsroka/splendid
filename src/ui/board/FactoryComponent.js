0da158f9ec2ab14a280338a5c13b8d9696c94f8d
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-09-14 13:58:43 +0200

    DRY: Use ResourceHelper.ReduceResources instead of local equivalent.

--- src/ui/board/FactoryComponent.js
+++ src/ui/board/FactoryComponent.js
@@ -2,18 +2,14 @@ import { connect } from 'react-redux';
 import Factory from './Factory.js';
 import RESOURCE_COLORS from '../ResourceColorMap.js';
 import ChooseFactoryFromBoardAction from '../../actions/ChooseFactoryFromBoardAction.js';
+import { ReduceResources } from '../../model/ResourceHelper.js';
 
 function mapStateToProps(state, ownProps) {
   let boardState = state.gameState.board;
   let resourceFactory = boardState.factoriesByRow[ownProps.rowIndex][ownProps.itemIndex];
 
   let bgColor = RESOURCE_COLORS[resourceFactory.color];
-  let costColors = resourceFactory.cost.sort().reduce(
-      (combinedCost, nextCost) => {
-        combinedCost[nextCost] = combinedCost.hasOwnProperty(nextCost) ? (combinedCost[nextCost] + 1) : 1;
-        return combinedCost;
-      },
-      {});
+  let costColors = ReduceResources(resourceFactory.cost);
 
   let selection = boardState.selection.type === 'FACTORY_SELECTION' ? boardState.selection : null;
 
