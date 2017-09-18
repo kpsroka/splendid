3d65234b072bbd62ee19f31f752bb6e5b0d2d5ce
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-09-18 16:17:52 +0200

    Reimplemeting LoadStateAction as a thunk action, using sessionstorage to restore game data.

--- src/actions/LoadStateAction.js
+++ src/actions/LoadStateAction.js
@@ -1,7 +1,26 @@
-import { ActionTypes } from './Actions.js';
+import GetGameData from './GetGameDataAction.js';
+import SetUiMode from './SetUiModeAction.js';
+
+const SESSION_STORAGE_KEY = 'gameref';
 
 export default function LoadStateAction() {
-  return {
-    type: ActionTypes.LoadState
-  }
+  return (dispatch) => {
+    let hashGameId = location.hash.slice(1);
+
+    let sessionGameRefStr = sessionStorage.getItem(SESSION_STORAGE_KEY);
+    let sessionGameRef =
+        sessionGameRefStr === null ? undefined : JSON.parse(sessionGameRefStr);
+
+    if (sessionGameRef === undefined && hashGameId === '') {
+      dispatch(SetUiMode('WELCOME'));
+    } else if (sessionGameRef === undefined && hashGameId !== '') {
+      dispatch(SetUiMode('JOIN'));
+    } else if (sessionGameRef !== undefined
+        && (hashGameId === '' || hashGameId === sessionGameRef.id)) {
+      dispatch(GetGameData(sessionGameRef));
+    } else {
+      sessionStorage.removeItem(SESSION_STORAGE_KEY);
+      dispatch(SetUiMode('JOIN'));
+    }
+  };
 }
