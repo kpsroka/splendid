99c552347d6181976d78a74e740fd750b92ec349
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-09-18 11:40:01 +0200

    Removing unused field from SetGameConfig action.

--- src/actions/SetGameConfigAction.js
+++ src/actions/SetGameConfigAction.js
@@ -4,7 +4,6 @@ export default function SetGameConfig(serverGameConfig) {
   return {
     type: ActionTypes.SetGameConfig,
     gameRef: serverGameConfig.ref,
-    gameId: serverGameConfig.ref.gameId,
     players: serverGameConfig.players,
   };
 }
