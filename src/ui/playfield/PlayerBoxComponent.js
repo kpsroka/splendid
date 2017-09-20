c860bfbda64505e393e5c11f1268840a8be497c4
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-09-20 15:45:33 +0200

    Displaying proper score in player's box.

--- src/ui/playfield/PlayerBoxComponent.js
+++ src/ui/playfield/PlayerBoxComponent.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import PlayerBox from './PlayerBox.js';
 
 import type { PlayerBoxProps, PlayerBoxOwnProps } from './PlayerBox.js';
-import type { State } from '../../model/State.js';
+import type { State, GameState } from '../../model/State.js';
 
 function mapStateToProps(state:State, ownProps:PlayerBoxOwnProps):PlayerBoxProps {
   if (state.players.length <= ownProps.playerIndex) {
@@ -13,8 +13,17 @@ function mapStateToProps(state:State, ownProps:PlayerBoxOwnProps):PlayerBoxProps
     return {
       disabled: false,
       name: state.players[ownProps.playerIndex].name,
-      score: 0
+      score: getScore(state.gameState, ownProps.playerIndex),
     };
+  }
+}
+
+function getScore(gameState:?GameState, playerIndex:number):number {
+  if (gameState) {
+    let playerHand = gameState.playerState[playerIndex].hand;
+    return playerHand.factories.reduce((sum, next) => (sum + next.points), 0);
+  } else {
+    return 0;
   }
 }
 
