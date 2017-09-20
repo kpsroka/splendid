f681175ea479817a26aefd27d69ff1f2189764a8
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-09-20 14:05:07 +0200

    Adding resource icons, and using them in PlayerResourceSupply.

---
+++ src/ui/ResourceIconMap.js
@@ -0,0 +1,9 @@
+const RESOURCE_ICONS = [
+  /* 0 */ "/resource_fire_64.png",
+  /* 1 */ "/resource_earth_64.png",
+  /* 2 */ "/resource_water_64.png",
+  /* 3 */ "/resource_air_64.png",
+  /* 4 */ "/resource_aether_64.png"
+];
+
+export default RESOURCE_ICONS;
--- src/ui/playfield/PlayerResourceSupply.js
+++ src/ui/playfield/PlayerResourceSupply.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import './PlayerResourceSupply.css';
 import RESOURCE_COLORS from '../ResourceColorMap.js';
+import RESOURCE_ICONS from '../ResourceIconMap.js';
 
 import type { Resource } from '../../model/State.js';
 
@@ -25,6 +26,11 @@ export default function PlayerResourceSupply(props:PlayerResourceSupplyCombinedP
              backgroundColor: RESOURCE_COLORS[props.resource],
              color: RESOURCE_COLORS[props.resource],
            }}>
+        <div className="PlayerResourceSupply-resourceIcon"
+             style={{
+               backgroundImage: `url('${RESOURCE_ICONS[props.resource]}')`
+             }}
+        ></div>
         <div className="PlayerResourceSupply-factoryCount">{props.factoryCount}</div>
         <div className="PlayerResourceSupply-resourceCount">{props.resourceCount}</div>
       </div>
