bf796b231934205701f97474e8b48cbadcdbc93b
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-09-26 12:38:19 +0200

    using dynamic "PUBLIC_URL" prefix in icon paths.

--- src/ui/ResourceIconMap.js
+++ src/ui/ResourceIconMap.js
@@ -1,9 +1,11 @@
+const PUBLIC_URL = process.env.PUBLIC_URL;
+
 const RESOURCE_ICONS = [
-  /* 0 */ "/resource_fire_64.png",
-  /* 1 */ "/resource_earth_64.png",
-  /* 2 */ "/resource_water_64.png",
-  /* 3 */ "/resource_air_64.png",
-  /* 4 */ "/resource_aether_64.png"
+  /* 0 */ `${PUBLIC_URL}/resource_fire_64.png`,
+  /* 1 */ `${PUBLIC_URL}/resource_earth_64.png`,
+  /* 2 */ `${PUBLIC_URL}/resource_water_64.png`,
+  /* 3 */ `${PUBLIC_URL}/resource_air_64.png`,
+  /* 4 */ `${PUBLIC_URL}/resource_aether_64.png`
 ];
 
 export default RESOURCE_ICONS;
